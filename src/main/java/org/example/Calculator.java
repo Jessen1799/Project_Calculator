f97ed123eb90a0a1f3d364d80ddbf18427c17b7e
Mads Krabbe <spiligen1@gmail.com>
2025-11-10 11:03:25 +0100

    2

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,11 +2,8 @@ package org.example;
 
 public class Calculator
 {
-    public void addNumbers(int num1, int num2)
-    {
-        System.out.println(num1 + num2);
-    }
-
+    public void addNumbers(int num1, int num2) {System.out.println(num1 + num2);}
+    public void add3Numbers(int num1, int num2, int num3) {System.out.println(num1 + num2 + num3);}
     /*
     Method to subtract two declared variables
      */
@@ -17,11 +14,5 @@ public class Calculator
     }
 
 }
-    public void add3Numbers(int num1, int num2, int num3)
-    {
-        System.out.println(num1 + num2 + num3);
-    }
-    //Hi,.,mhgfjyf
-}
-    //Hi
-}
+
+
