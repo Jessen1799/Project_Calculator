4034b400cc69d139e739751b6a93b1013c6fb93b
Mikkelno1 <mikkelnorinder@live.dk>
2025-11-10 10:35:25 +0100

    Added subtraction method

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,4 +3,13 @@ package org.example;
 public class Calculator
 {
 
+    public void subtractNumbers()
+    {
+        int num1 = 4;
+        int num2 = 6;
+        int sum = num1 - num2;
+
+        System.out.println(sum);
+    }
+
 }
