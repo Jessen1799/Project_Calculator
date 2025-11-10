b47203e28cecf1bca9d2c14ec5e454b9db778bf7
Mads Krabbe <spiligen1@gmail.com>
2025-11-10 10:59:47 +0100

    kvgtrf

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -15,11 +15,11 @@ public class Calculator
         int sum = num1 - num2;
         System.out.println(sum);
     }
-
-}
     public void add3Numbers(int num1, int num2, int num3)
     {
         System.out.println(num1 + num2 + num3);
     }
     //Hi
 }
+
+
