91315a3ac81a7bcaf3437f01155fec9e6a1b6fdd
Mads Krabbe <spiligen1@gmail.com>
2025-11-10 10:37:03 +0100

    added addNumbers og add3numbers

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,5 +2,13 @@ package org.example;
 
 public class Calculator
 {
+    public void addNumbers(int num1, int num2)
+    {
+        System.out.println(num1 + num2);
+    }
 
+    public void add3Numbers(int num1, int num2, int num3)
+    {
+        System.out.println(num1 + num2 + num3);
+    }
 }
